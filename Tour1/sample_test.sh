e23f5d96df9538e0f3f2e68c8a07023848a7996d
Pai, Cary <cary.pai@intel.com>
2019-02-25 23:17:56 +0800

    Change the test file name in sample_test.sh and change the calibration iteration time from 500 to 200

--- Tour1/sample_test.sh
+++ Tour1/sample_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /opt/intel/computer_vision_sdk/bin/setupvars.sh
-./object_detection_sample_ssd -i /content/004545.jpg -m /content/MobileNetSSD_FP32/MobileNetSSD_deploy.xml
+./object_detection_sample_ssd -i /content/horse.jpg -m /content/MobileNetSSD_FP32/MobileNetSSD_deploy.xml
